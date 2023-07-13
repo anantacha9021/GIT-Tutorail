3d6a4c1edbc2bad5599cb102ab0d89a0150c8868
anantacha9021 <138754922+anantacha9021@users.noreply.github.com>
2023-07-13 14:39:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,32 +2,4 @@
 <?php each "Anantacha khamyuta"?>
 <?php each "6511850011"?>
 <?php each "mut"?>
-<?php each "mut"?>[33mcommit 3e426154ef5bb7f0cdcca8aace5508f12187207e[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
-Author: anantacha9021 <anantacha9021@gmail.com>
-Date:   Thu Jul 13 14:23:18 2023 +0700
-
-     cot
-
-[33mcommit e9028b141cda570bfc1078c1995612bdcac68689[m
-Author: anantacha9021 <anantacha9021@gmail.com>
-Date:   Thu Jul 13 14:19:53 2023 +0700
-
-     commit
-
-[33mcommit f18f3ad68bfe0dbca89e28b5947653d60c4393a6[m
-Author: anantacha9021 <anantacha9021@gmail.com>
-Date:   Thu Jul 13 14:13:39 2023 +0700
-
-    frist commit
-
-[33mcommit c804b060d0539fdc1821562f89967cff6695fe6f[m
-Author: anantacha9021 <anantacha9021@gmail.com>
-Date:   Thu Jul 13 14:03:59 2023 +0700
-
-    frist commit
-
-[33mcommit 5a55f4b5cfa81966f611c3144828487c337d39d3[m
-Author: anantacha9021 <anantacha9021@gmail.com>
-Date:   Thu Jul 13 13:46:41 2023 +0700
-
-    frist commit
+<?php each "mut"?>
